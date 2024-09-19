a8e6dc057e907a42ae99b7a28ed023b54c050ed1
userDanielGomez <101981448+userDanielGomez@users.noreply.github.com>
2024-09-18 23:37:07 -0600

    Se agrega ejercicio 6

---
+++ src/main/java/com/mycompany/ejercicios/Ejercicio6InversorPalabras.java
@@ -0,0 +1,36 @@
+package com.mycompany.ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author danie
+ */
+public class Ejercicio6InversorPalabras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Generar código que solicite al usuario una palabra y posteriormente 
+        // muestre en consola la palabra invertida usando los metodos
+        // .lenth y .substring
+        String palabra;
+        
+        //Declaración objeto Scanner para lectura de teclado
+        Scanner teclado = new Scanner(System.in);
+        
+        //Solicitud y lectura de variable palabra
+        System.out.println("Ingrese la palabra que desea invertir");
+        palabra = teclado.nextLine();
+        
+        //Arreglo que usa .substring para obtener la cadena invertida
+        for(int i = palabra.length(); i>0; i--){
+            System.out.print(palabra.substring(i-1,i));
+        }
+        
+        //Se cierra el objeto Scanner
+        teclado.close();
+        
+    }
+}
